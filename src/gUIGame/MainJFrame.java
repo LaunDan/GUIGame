e4838869629c136d8e76ca42a8e974c3c832cf9f
LaunDan <launadaniel2@gmail.com>
2020-04-01 21:14:35 +0200

    set class Mypanel

--- src/gUIGame/MainJFrame.java
+++ src/gUIGame/MainJFrame.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class MainJFrame extends JFrame {
 
-    public MainJFrame(){
+    public MainJFrame() {
         this.setTitle("Display image");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
--- src/gUIGame/MyPanel.java
+++ src/gUIGame/MyPanel.java
@@ -1,7 +1,22 @@
 package gUIGame;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MyPanel extends JPanel {
+    private Image picture;
 
+    public MyPanel() {
+    this.setPreferredSize(new Dimension(1050,800));
+    this.setBackground(Color.white);
+
+    ImageIcon ii = new ImageIcon(this.getClass().getResource("picture1.jpg"));
+    picture = ii.getImage();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        g.drawImage(picture, 5 , 5, this);
+    }
 }
