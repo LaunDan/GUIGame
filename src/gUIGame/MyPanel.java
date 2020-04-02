9e513e86ef3dda8bef78b13df952a27de2195df2
LaunDan <launadaniel2@gmail.com>
2020-04-02 19:47:37 +0200

    set dimension of panel

--- src/gUIGame/MyPanel.java
+++ src/gUIGame/MyPanel.java
@@ -9,18 +9,26 @@ public class MyPanel extends JPanel {
     private int pictureHeight;
 
     public MyPanel() {
-    this.setPreferredSize(new Dimension(1050,800));
-    this.setBackground(Color.white);
 
-    ImageIcon ii = new ImageIcon(this.getClass().getResource("picture1.jpg"));
-    picture = ii.getImage();
+        ImageIcon ii = new ImageIcon(this.getClass().getResource("picture1.jpg"));
+        picture = ii.getImage();
 
-    
+        pictureWidth = picture.getWidth(this);
+        pictureHeight = picture.getHeight(this);
+
+        int x = 20 + pictureWidth + 20;
+        int y = 20 + pictureHeight + 20;
+
+        Dimension dimensionPanel = new Dimension(x,y);
+
+        this.setPreferredSize(dimensionPanel);
+        this.setBackground(Color.BLACK);
+
     }
 
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawImage(picture, 5 , 5, this);
+        g.drawImage(picture, 20, 20, this);
     }
 }
